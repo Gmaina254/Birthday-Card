e5510bfd0b5f3fb1e33997c80dcfcbac0d6088c2
GIDEON MAINA <mutondogm@gmail.com>
2022-06-10 15:36:34 +0300

    Birthday card

---
+++ app/src/main/java/com/example/birthdaycard/MainActivity.kt
@@ -0,0 +1,69 @@
+package com.example.birthdaycard
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.birthdaycard.ui.theme.BirthdayCardTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BirthdayCardTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    BirthdayImage("Happy Birthday bro", from = "~from Faze~")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun BirthdayImage(message : String, from: String) {
+    val image = painterResource(id = R.drawable.androidparty)
+    Box {
+        Image(painter = image, contentDescription = null
+        , modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+        , contentScale = ContentScale.Crop
+        )
+        BirthdayMessage(message = message, from = from)
+    }
+}
+@Composable
+fun BirthdayMessage(message : String, from : String) {
+    Column {
+        Text(text = message, fontSize = 36.sp,
+        modifier = Modifier.fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally)
+            .padding(start = 16.dp, top = 16.dp))
+        Text(text = from, fontSize = 20.sp,
+            modifier = Modifier.fillMaxWidth().wrapContentWidth(Alignment.CenterHorizontally)
+                .padding(start = 16.dp, end = 16.dp))
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BirthdayCardTheme {
+        BirthdayImage("Happy Birthday bro", from = "~from Faze~")
+    }
+}
